bb79a612a8f1822673fbc154c1d8565712802f9e
Alistair Buxton <a.j.buxton@gmail.com>
2012-11-06 12:58:55 +0000

    Add license.

--- console.c
+++ console.c
@@ -1,3 +1,22 @@
+/*
+    Copyright 2010 Alistair Buxton <a.j.buxton@gmail.com>
+
+    This file is part of avr-teletext.
+
+    avr-teletext is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    avr-teletext is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with avr-teletext.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <inttypes.h>
 #include <avr/io.h>
 #include <avr/wdt.h>
